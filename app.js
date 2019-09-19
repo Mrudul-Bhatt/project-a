82a77e6e22d0e97a6f5dcb1171ba666c68e7560a
Mrudul-Bhatt <mrudul1203bhatt@gmail.com>
2019-09-19 05:55:57 +0530

    added cart

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('5d821abb857e1843e4546433')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
